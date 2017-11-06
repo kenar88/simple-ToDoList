faf27b0abb5661e71402bffda34c0a3f5c9c95b9
kenar88 <roos1788@gmail.com>
2017-11-06 15:01:54 +0500

    03

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let myNodeList = document.getElementsByTagName('li');
+
+let close = document.getElementsByClassName('close');
+
+let list = document.getElementById('mainUl');
+
+let add = document.getElementById('add');
+
 for (let i = 0; i < myNodeList.length; i++) {
   let span = document.createElement('span');
   let txt = document.createTextNode('\u00D7');
@@ -7,7 +14,7 @@ for (let i = 0; i < myNodeList.length; i++) {
   myNodeList[i].appendChild(span);
 }
 
-let close = document.getElementsByClassName('close');
+
 for (let i = 0; i < close.length; i++) {
   close[i].onclick = function () {
     let div = this.parentElement;
@@ -16,22 +23,37 @@ for (let i = 0; i < close.length; i++) {
 
 }
 
-let list = document.getElementById('mainUl');
+
 list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
-  ev.target.classList.toggle('checked');
-}
+    ev.target.classList.toggle('checked');
+  }
 }, false);
 
-let add = document.getElementById('add');
+
 add.onclick = () => {
   let li = document.createElement('li');
   let inputValue = document.getElementById('mainInput').value;
   let inputValueTxt = document.createTextNode(inputValue);
+  let span = document.createElement('span');
+  let txt = document.createTextNode('\u00D7');
+  span.className = 'close';
+  span.appendChild(txt);
+  li.appendChild(span);
   li.appendChild(inputValueTxt);
-  if(inputValue == '') {
+  if (inputValue == '') {
     alert('This field cannot be empty');
-  }else {
+  } else {
     document.getElementById('mainUl').appendChild(li);
   }
-} 
+
+  document.getElementById('mainInput').value = '';
+  
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.hidden = true;
+    }
+
+  }
+}
